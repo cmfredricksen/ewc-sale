bc7dfb329730cd930e65d886ddf7799820f4427e
Christine Fredricksen <cmfredricksen@gmail.com>
2022-03-11 16:55:39 -0600

    added about and directions pages

---
+++ src/routes/about.svelte
@@ -0,0 +1,25 @@
+<div class="about">
+	<h2>About Us</h2>
+
+	<p>The Ely Watercolor Club was founded in 1985 by local artists in Ely Minnesota.</p>
+
+	<p>
+		We are a group of independent artists who primarily focus on watercolor painting. We have weekly
+		painting sessions at a local church and we have our annual show and sale every summer.
+	</p>
+
+	<p>
+		Many of us also participate in other local artist groups and our art can be found around town in
+		many businesses and at <a href="https://elysartandsoul.com/">Art & Soul Gallery</a>.
+	</p>
+</div>
+
+<style>
+	.about {
+		padding: 1.5rem;
+	}
+
+	p {
+		margin: 1rem 0;
+	}
+</style>
---
+++ src/routes/directions.svelte
@@ -0,0 +1,9 @@
+<div class="directions">
+	<h2>How to find us!</h2>
+</div>
+
+<style>
+	.directions {
+		padding: 1.5rem;
+	}
+</style>
