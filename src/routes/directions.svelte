5aa754bcc67cfca493d4a7617a990caaee4f0f61
Chris Fredricksen <cmfredricksen@gmail.com>
2022-06-14 09:12:55 -0500

    fixed map

--- src/routes/directions.svelte
+++ src/routes/directions.svelte
@@ -2,44 +2,16 @@
 	<title>Ely Watercolor Club | Directions</title>
 </svelte:head>
 
-<div class="directions">
+
+<div class="new-map directions">
 	<h2>How to find us!</h2>
-
+	
 	<h2>401 N. Pioneer Road, Ely MN</h2>
+	
 
-	<div class="map-box">
-		<div class="mapouter">
-			<div class="gmap_canvas">
-				<iframe
-					width="600"
-					height="500"
-					id="gmap_canvas"
-					src="https://maps.google.com/maps?q=401%20N.%20Pioneer%20Rd&t=&z=13&ie=UTF8&iwloc=&output=embed"
-					frameborder="0"
-					scrolling="no"
-					marginheight="0"
-					marginwidth="0"
-					title="map"
-				/><a href="https://putlocker-is.org">putlocker</a><br /><style>
-					.mapouter {
-						position: relative;
-						text-align: right;
-						height: 500px;
-						width: 600px;
-					}
-				</style><a href="https://www.embedgooglemap.net">add google maps html</a><style>
-					.gmap_canvas {
-						overflow: hidden;
-						background: none !important;
-						height: 500px;
-						width: 600px;
-					}
-				</style>
-			</div>
-		</div>
-	</div>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d42788.30590854065!2d-91.860923!3d47.91183!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52a5affc1e73c309%3A0xdeb57b115bb8d891!2s401%20N%20Pioneer%20Rd%2C%20Ely%2C%20MN%2055731!5e0!3m2!1sen!2sus!4v1655215408792!5m2!1sen!2sus" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="directions to the sale"></iframe>
+	
 </div>
-
 <style>
 	.directions {
 		padding: 2rem 1.5rem;
@@ -56,22 +28,6 @@
 
 		h2 {
 			text-align: center;
-		}
-		.map-box {
-			text-align: center;
-			overflow: auto;
-			margin-top: 2.5rem;
-			padding-bottom: 95%;
-			position: relative;
-			height: 0;
-		}
-		.map-box iframe {
-			text-align: center;
-			left: 0;
-			top: 0;
-			height: 100%;
-			width: 100%;
-			position: absolute;
 		}
 	}
 </style>
